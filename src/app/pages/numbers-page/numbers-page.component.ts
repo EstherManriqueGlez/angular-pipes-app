280a20df64b8f24a9907b30b0115736cc84c7680
Esther Manrique <esthermanriqueepam@gmail.com>
2025-10-19 14:40:41 -0600

    refactor: restructure app layout and add navigation bar component; enhance basic and numbers pages with pipes examples

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.component.html',
 })
-export default class NumbersPageComponent { }
+export default class NumbersPageComponent {
+
+  totalSales = signal(2_567_889.5567);
+  percentage = signal(0.4856);
+ }
