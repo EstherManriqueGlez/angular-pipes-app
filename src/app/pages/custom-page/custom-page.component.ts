595a1bbb6773166681cdc4f8e31c1d1c4f5f7d51
Esther Manrique <esthermanriqueepam@gmail.com>
2025-10-25 14:04:52 -0600

    feat: add hero filter and sort by pipes for enhanced hero data manipulation

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -4,9 +4,11 @@ import { Hero } from '../../interfaces/hero.interface';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
-import { HeroTextColorPipe } from "../../pipes/hero-text-color.pipe";
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -17,11 +19,12 @@ import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
     HeroTextColorPipe,
     TitleCasePipe,
     HeroCreatorPipe,
-],
+    HeroSortByPipe,
+    HeroFilterPipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
-
   name = signal<string>('Alberto Garcia');
 
   upperCase = signal<boolean>(true);
@@ -32,4 +35,21 @@ export default class CustomPageComponent {
   }
 
   heroes = signal<Hero[]>(heroes);
+
+  sortBy = signal<keyof Hero | null>(null);
+
+  sortDirection = signal<'asc' | 'desc'>('asc');
+
+  searchQuery = signal<string>('');
+
+  onSortByChange(value: keyof Hero | null) {
+    if (this.sortBy() === value) {
+      // Si es la misma columna, cambiar dirección
+      this.sortDirection.set(this.sortDirection() === 'asc' ? 'desc' : 'asc');
+    } else {
+      // Si es diferente columna, empezar con ascendente
+      this.sortBy.set(value);
+      this.sortDirection.set('asc');
+    }
+  }
 }
